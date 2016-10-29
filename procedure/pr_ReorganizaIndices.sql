dd2aede452d0d7a68a12372ee138398308aaed89
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2016-10-29 14:55:44 -0200

    Nova procedure
    Procedure responsável por reorganizar todos os índices do banco de dados ativo.

---
+++ procedure/pr_ReorganizaIndices.sql
@@ -0,0 +1,46 @@
+IF (OBJECT_ID('pr_ReorganizaIndices','P') IS NOT NULL)
+    DROP PROCEDURE pr_ReorganizaIndices
+GO
+
+CREATE PROCEDURE pr_ReorganizaIndices
+
+AS
+
+BEGIN
+
+	DECLARE @cSQL     varchar(400)
+		   ,@cSchema  varchar(20)
+		   ,@cNmTable varchar(150)
+
+	DECLARE curInReorganize CURSOR
+		FOR SELECT TABLE_SCHEMA
+				  ,TABLE_NAME
+			  FROM information_schema.tables 
+			 WHERE TABLE_TYPE = 'BASE TABLE'
+
+	OPEN curInReorganize
+
+	FETCH NEXT 
+	 FROM curInReorganize
+	 INTO @cSchema
+		 ,@cNmTable
+
+	WHILE (@@FETCH_STATUS = 0)
+	BEGIN
+
+		PRINT 'Reorganizando índices do objeto ' + @cNmTable + '...'
+
+		SET @cSQL = CONCAT('ALTER INDEX ALL ON ', DB_NAME(), '.', @cSchema, '.', @cNmTable, ' REORGANIZE')
+		EXEC(@cSQL)
+
+		FETCH NEXT 
+		 FROM curInReorganize
+		 INTO @cSchema
+			 ,@cNmTable
+
+	END
+
+	CLOSE curInReorganize
+	DEALLOCATE curInReorganize
+
+END
